e3c395605251bbcaae36b5c4b8676f03f9b60391
ScarLi <yigitcagri48@gmail.com>
2025-04-12 19:19:16 +0300

    can be delete now

--- todoManager.js
+++ todoManager.js
@@ -5,9 +5,16 @@ import { createProject } from './project.js';
 const projects = [createProject('Default')];
 
 function addTodoToProject(todoData, projectId) {
-  const todo = createTodo(todoData);
-  const project = projects.find(p => p.id === projectId);
-  if (project) {
-    project.todos.push(todo);
-  }
+    const todo = createTodo(todoData);
+    const project = projects.find(p => p.id === projectId);
+    if (project) {
+        project.todos.push(todo);
+    }
+}
+
+function deleteTodoFromProject(todoId, projectId) {
+    const project = projects.find(p => p.id === projectId);
+    if (project) {
+        project.todos = project.todos.filter(todo => todo.id !== todoId);
+    }
 }
